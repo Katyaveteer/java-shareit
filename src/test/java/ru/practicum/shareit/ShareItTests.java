fa627cadca1a4dcc4fd0217a9adc1d3b8739d2ac
katyaveteer <Katyavetee@yandex.ru>
2025-09-26 23:12:59 +0300

    добавлена работа с базой данных в уже реализованную часть проекта, а также реализована возможность бронирование вещей

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
